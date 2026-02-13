7f85381f3da9c42adc53d299a7231c8ab68f0211
TonyHebe <125080921+TonyHebe@users.noreply.github.com>
2026-02-13 14:37:40 +0200

    Add Multitag ad integration - Update sw.js with new zone ID and add Multitag script to all HTML pages

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10581806
+    "zoneId": 10605969
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
